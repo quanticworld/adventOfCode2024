37bc1d4bb0ba14797c21227178a24b791e80c03a
quantic <alexis.tresso@proton.me>
2024-12-01 22:24:07 +0100

    feat: day 1 - optim part2

--- day1/code.py
+++ day1/code.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def part1():
     l1, l2 = map(list, zip(*[row.split('   ') for row in open("day1/data1.txt", "r").readlines()]))
     l1.sort(), l2.sort()
@@ -8,6 +11,6 @@ def part2():
     l1, l2 = map(list, zip(*[row.split('   ') for row in open("day1/data2.txt", "r").readlines()]))
     l1 = [int(e) for e in l1]
     l2 = [int(e) for e in l2]
-    l_dict = {key: l2.count(key) for key in set(l2)}
+    l_dict = Counter(l2)
 
     return sum([0 if e not in l_dict else e * l_dict[e] for e in l1])
