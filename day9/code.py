bddbf0d8a4542085593e56c90b79db4bf419fa0e
Alexis Tresso <atresso@hellowork.com>
2024-12-09 13:27:33 +0100

    day9 part1 ok

--- day9/code.py
+++ day9/code.py
@@ -1,18 +1,24 @@
 from itertools import chain, groupby
 
+import numpy
+
+
 def expand_from_allocation_table(allocation_table):
-    result = list(chain.from_iterable([[f_or_s[0]//2] * int(f_or_s[1]) if f_or_s[0] % 2 == 0 else [-1] * int(f_or_s[1]) for f_or_s in enumerate(allocation_table)]))
+    result = numpy.array(list(chain.from_iterable([[f_or_s[0]//2] * int(f_or_s[1]) if f_or_s[0] % 2 == 0 else [-1] * int(f_or_s[1]) for f_or_s in enumerate(allocation_table)])))
     return result
 
+def np_index(array, value):
+    return numpy.where(array == value)[0][0]
+
 def defragment_disk(expanded_data):
-    last_index_of_empty_space = len(expanded_data) - 1 - expanded_data[::-1].index(-1)
+    last_index_of_empty_space = len(expanded_data) - 1 - np_index(expanded_data[::-1], -1)
     for idx_right, _ in reversed(list(enumerate(expanded_data))):
         remaining_spaces = -1 in expanded_data
 
         if not remaining_spaces:
             return expanded_data
 
-        idx_left = expanded_data.index(-1)
+        idx_left = np_index(expanded_data, -1)
 
 
         swap_and_replace_right(expanded_data, idx_left, idx_right, -2)
@@ -20,7 +26,7 @@ def defragment_disk(expanded_data):
     return expanded_data
 
 def compute_checksum(data):
-    return sum([idx * value for idx, value in enumerate(data[:data.index(-2)])])
+    return sum([idx * value for idx, value in enumerate(data[:np_index(data, -2)])])
 
 
 def group_repeated_values(data):
