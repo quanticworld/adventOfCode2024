13f9cd6183dd528ef7fe843fafcfd8659ebdb23f
quantic <alexis.tresso@proton.me>
2024-12-11 21:54:23 +0100

    day 11 - vive python

--- day11/code.py
+++ day11/code.py
@@ -1,33 +1,33 @@
-from collections import defaultdict, deque
-from pprint import pprint
+from collections import deque
+from functools import lru_cache
+
+
+# Ouaiiii ouaiiii c'est limite limite, mais ça sert à ça, la memoisation hein !
+@lru_cache(None)
+def calc_next(value, current_iter, max_iter, accu):
+    if current_iter == max_iter:
+        return 1
+
+    if value == 0:
+        return calc_next(1, current_iter + 1, max_iter, accu)
+    elif len(str(value)) % 2 == 0:
+        str_value = str(value)
+        return (calc_next(int(str_value[:len(str_value) // 2]), current_iter + 1, max_iter, accu)
+                + calc_next(int(str_value[len(str_value) // 2:]), current_iter + 1, max_iter, accu))
+    else:
+        return calc_next(value * 2024, current_iter + 1, max_iter, accu + 1)
 
 
 def solve(iterations):
-    stones = deque([int(stone) for stone in open("day11/sample.txt", "r").read().split(' ')])
-
-    for i in range(iterations):
-        new_stones = deque()
-        for stone_idx in range(len(stones)):
-
-            if stones[stone_idx] == 0:
-                new_stones.append(1)
-
-            elif len(str(stones[stone_idx])) % 2 == 0:
-                str_stone = str(stones[stone_idx])
-                first_stone = int(str_stone[:len(str_stone) // 2])
-                second_stone = int(str_stone[len(str_stone) // 2:])
-                new_stones.append(second_stone)
-                new_stones.append(first_stone)
-
-            else:
-                new_stones.append(stones[stone_idx] * 2024)
-        stones = new_stones
-        a = 1
-    return len(stones)
+    stones = deque([int(stone) for stone in open("day11/data.txt", "r").read().split(' ')])
+    return sum(list(map(lambda value: calc_next(value, 0, iterations, 0), stones)))
 
 
 def part1():
-    return solve(25)
+    result = solve(25)
+    return result
+
 
 def part2():
-    return solve(75)
+    result = solve(75)
+    return result
